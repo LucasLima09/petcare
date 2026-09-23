df5a51fabeebb2a2dac1b13d49438f44737a8677
Lucas Ferreira de Lima <ferreiradelimalucas09@gmail.com>
2026-09-23 08:16:31 -0300

    art: adiciona css no modal

--- src/components/sexoButtonComponent/index.tsx
+++ src/components/sexoButtonComponent/index.tsx
@@ -13,37 +13,31 @@ export function SexoSelector({ value, onChange }: Props) {
     const [selected, setSelected] = useState<boolean>(false);
 
     return (
-        <View>
-            <TouchableOpacity
-                style={selected && styles.cardSelected}
-                onPress={() => { onChange(value), setSelected(!selected) }}
-                activeOpacity={0.8}
-            >
-                <Text>{value}</Text>
-            </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+            style={[styles.card, selected && styles.cardSelected]}
+            onPress={() => { onChange(value), setSelected(!selected) }}
+            activeOpacity={0.8}
+        >
+            <Text style={[styles.text, selected && styles.textSelected]}>{value}</Text>
+        </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        gap: 12,
-        marginVertical: 8,
-    },
     card: {
         flex: 1,
         paddingVertical: 14,
         borderRadius: 10,
         borderWidth: 1.5,
+        width: "100%",
         borderColor: '#E2E8F0',
         backgroundColor: '#F8FAFC',
         alignItems: 'center',
         justifyContent: 'center',
     },
     cardSelected: {
-        borderColor: '#0284C7', // Cor de destaque (Ex: Azul/Tema do app)
-        backgroundColor: '#E0F2FE', // Fundo levemente colorido
+        borderColor: '#0284C7',
+        backgroundColor: '#E0F2FE',
     },
     text: {
         fontSize: 16,
--- src/screens/homeScreen.tsx
+++ src/screens/homeScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, TextInput, View } from "react-native";
+import { Button, Modal, TextInput, View, StyleSheet, Pressable, Text } from "react-native";
 import { logoutUser } from "../services/authService";
 import { useState } from "react";
 import { Pet } from "../types/pet";
@@ -40,32 +40,62 @@ export default function HomeScreen() {
                 onPress={() => setIsModalVisible(true)}
             />
 
-
             <Modal
                 visible={isModalVisible}
                 onRequestClose={() => setIsModalVisible(false)}
                 animationType="slide"
             >
-                <View style={{ flex: 1, padding: 60 }}>
-                    <TextInput placeholder="Nome do Pet" value={nomePet} onChangeText={setNomePet} />
-                    <TextInput placeholder="Raça" value={raca} onChangeText={setRaca} />
-                    <TextInput placeholder="Data de Nascimento" value={dataNascimento} onChangeText={setDataNascimento} />
-                    <TextInput placeholder="Peso" value={peso} onChangeText={setPeso} />
-                    <SexoSelector
-                        value="Macho"
-                        onChange={() => setSexo("Macho")}
-                    />
-                    <SexoSelector
-                        value="Fêmea"
-                        onChange={() => setSexo("Fêmea")}
-                    />
-                    <TextInput placeholder="Observação" value={observacao} onChangeText={setObservacao} />
-                    <Button
-                        title="Adicionar"
+                <View style={{ flex: 1, padding: 30, gap: 20 }}>
+                    <TextInput style={styles.input} placeholder="Nome do Pet" placeholderTextColor={"grey"} value={nomePet} onChangeText={setNomePet} />
+                    <TextInput style={styles.input} placeholder="Raça" placeholderTextColor={"grey"} value={raca} onChangeText={setRaca} />
+                    <TextInput style={styles.input} placeholder="Data de Nascimento" placeholderTextColor={"grey"} value={dataNascimento} onChangeText={setDataNascimento} />
+                    <TextInput style={styles.input} placeholder="Peso" placeholderTextColor={"grey"} value={peso} onChangeText={setPeso} />
+                    <View style={{ flexDirection: "row", gap: 20, alignItems: "center", justifyContent: "center" }}>
+                        <SexoSelector
+                            value="Macho"
+                            onChange={() => setSexo("Macho")}
+                        />
+                        <SexoSelector
+                            value="Fêmea"
+                            onChange={() => setSexo("Fêmea")}
+                        />
+                    </View>
+                    <TextInput style={styles.input} placeholder="Observação" placeholderTextColor={"grey"} value={observacao} onChangeText={setObservacao} />
+                    <Pressable
+                        style={styles.botao}
                         onPress={() => adicionarPet()}
-                    />
+                    ><Text style={styles.botaoLetra}>Adicionar</Text></Pressable>
                 </View>
             </Modal>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#0F4D0F",
+    },
+    input: {
+        backgroundColor: "#FFFFFF",
+        borderRadius: 12,
+        paddingHorizontal: 16,
+        paddingVertical: 14,
+        fontSize: 16,
+        borderWidth: 1,
+        borderColor: "#DDD",
+
+    },
+    botao: {
+        backgroundColor: "green",
+        borderRadius: 12,
+        paddingVertical: 16,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 10,
+    },
+    botaoLetra: {
+        color: "white",
+        fontSize: 18
+    }
+});
