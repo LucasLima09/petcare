ec18f7ec259d199f4213ac342bec673e01492343
Lucas Ferreira de Lima <ferreiradelimalucas09@gmail.com>
2026-09-25 14:32:42 -0300

    art: ajusta layout do card do pet para exibir a imagem

--- src/components/petCard/index.tsx
+++ src/components/petCard/index.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 
 type PetCardProps = {
     nome: string
@@ -10,15 +10,20 @@ type PetCardProps = {
     imagem?: string
 }
 
-export default function PetCard({ nome, raca, peso, sexo, dataNascimento, observacoes }: PetCardProps) {
+export default function PetCard({ nome, raca, peso, sexo, dataNascimento, observacoes, imagem }: PetCardProps) {
     return (
         <View style={styles.card}>
-            <Text style={styles.cardNome}>{nome}</Text>
-            <Text style={styles.cardInfo}>Raça: {raca}</Text>
-            <Text style={styles.cardInfo}>Peso: {peso}kg</Text>
-            <Text style={styles.cardInfo}>Sexo: {sexo}</Text>
-            <Text style={styles.cardInfo}>Nascimento: {dataNascimento}</Text>
-            {observacoes ? <Text style={styles.cardInfo}>Obs: {observacoes}</Text> : null}
+            <View style={styles.containerInfo}>
+                <Text style={styles.cardNome}>{nome}</Text>
+                <Text style={styles.cardInfo}>Raça: {raca}</Text>
+                <Text style={styles.cardInfo}>Peso: {peso}kg</Text>
+                <Text style={styles.cardInfo}>Sexo: {sexo}</Text>
+                <Text style={styles.cardInfo}>Nascimento: {dataNascimento}</Text>
+                {observacoes ? <Text style={styles.cardInfo}>Obs: {observacoes}</Text> : null}
+            </View>
+            <View style={styles.containerImagem}>
+                <Image style={styles.imagem} source={{ uri: imagem }} />
+            </View>
         </View>
     );
 }
@@ -27,15 +32,31 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor: "#FFFFFF",
         borderRadius: 12,
-        padding: 16,
         borderWidth: 1,
         borderColor: "#DDD",
         gap: 4,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        overflow: "hidden",
     },
     cardNome: {
         fontSize: 18,
         fontWeight: "bold",
         color: "#0F4D0F",
+    },
+    containerInfo: {
+        gap: 4,
+        padding: 16,
+    },
+    containerImagem: {
+        width: "35%",
+        height: "100%",
+        borderRadius: 12,
+    },
+    imagem: {
+        width: "100%",
+        height: "100%",
     },
     cardInfo: {
         fontSize: 14,
